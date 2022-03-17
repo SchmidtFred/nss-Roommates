a96e1556ed16e912e306bba5f9c0269145442f9f
SchmidtFred <fschmidt@live.com>
2022-03-17 14:36:43 -0500

    next commit

--- Roommates/Repositories/ChoreRepository.cs
+++ Roommates/Repositories/ChoreRepository.cs
@@ -86,5 +86,92 @@ namespace Roommates.Repositories
                 }
             }
         }
+
+        public List<Chore> GetUnassignedChores()
+        {
+            using (SqlConnection conn = Connection)
+            {
+                conn.Open();
+                using (SqlCommand cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText = @"SELECT c.Name, c.Id 
+                                        FROM Chore c 
+                                        LEFT JOIN RoommateChore rc ON rc.ChoreId = c.Id 
+                                        LEFT JOIN Roommate r ON rc.RoommateId = r.Id
+                                        WHERE rc.Id IS NULL";
+                    using (SqlDataReader reader = cmd.ExecuteReader())
+                    {
+                        List<Chore> chores = new List<Chore>();
+
+                        while (reader.Read())
+                        {
+                            string nameValue = reader.GetString(reader.GetOrdinal("Name"));
+                            int idValue = reader.GetInt32(reader.GetOrdinal("Id"));
+
+                            Chore chore = new Chore
+                            {
+                                Id = idValue,
+                                Name = nameValue
+                            };
+
+                            chores.Add(chore);
+                        }
+
+                        return chores;
+                    }
+                }
+            }
+        }
+
+        public void AssignChore(int roommateId, int choreId)
+        {
+            using (SqlConnection conn = Connection)
+            {
+                conn.Open();
+
+                using(SqlCommand cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText = @"INSERT INTO RoommateChore (ChoreId, RoommateId)
+                                                VALUES (@choreId, @roommateId)";
+                    cmd.Parameters.AddWithValue("@choreId", choreId);
+                    cmd.Parameters.AddWithValue("@roommateId", roommateId);
+                    cmd.ExecuteNonQuery();
+                }
+            }
+        }
+
+        public void Update(Chore chore)
+        {
+            using (SqlConnection conn = Connection)
+            {
+                conn.Open();
+
+                using (SqlCommand cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText = @"UPDATE Chore
+                                         SET Name = @name
+                                         WHERE Id = @id";
+                    cmd.Parameters.AddWithValue("@name", chore.Name);
+                    cmd.Parameters.AddWithValue("@id", chore.Id);
+
+                    cmd.ExecuteNonQuery();
+                }
+            }
+        }
+
+        public void Delete(int id)
+        {
+            using (SqlConnection conn = Connection)
+            {
+                conn.Open();
+
+                using (SqlCommand cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText = "DELETE FROM Room WHERE Id = @id";
+                    cmd.Parameters.AddWithValue("@id", id);
+                    cmd.ExecuteNonQuery();
+                }
+            }
+        }
     }
 }
